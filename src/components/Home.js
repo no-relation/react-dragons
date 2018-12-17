24b01d5134b55b308a1a5f0da794babb77544877
no-relation <eddiec76@gmail.com>
2018-12-17 09:19:37 -0600

    implemented search

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,38 +5,33 @@ class Home extends Component{
 
     constructor(props) {
         super(props)
-        console.log('inside the constructor',this.props.dragons)
         this.state = {
-            filteredDragons: this.props.dragons
+            query: ''
         }
     }
 
     searchFilter = (query) => {
-        console.log('query is',query)
         this.setState(state => {
-            console.log('filteredDragons has', this.state.filteredDragons)
-            this.setState = (state => {
-                state.filteredDragons = this.props.dragons.filter(dragon =>{
-                    const result = dragon.name.search(query)
-                    if (result === -1) {
-                        return false
-                    } else {
-                        return true
-                    }
-                })
-                return state
-            })    
+            state.query = query
+            return state
         })
     }
 
     render(){
+        let filteredDragons = this.props.dragons.filter(dragon => {
+            const result = dragon.name.toLowerCase().search(this.state.query.toLowerCase())
+            if (result === -1) {
+                return false
+            } else {
+                return true
+            }
+        })    
         // Note how style attributes are now passed as object instead of a string!!!
-        console.log('in the render', this.props.dragons)
         return (
         <div style={{float:'left',width:'40%', padding:'5%', backgroundColor:'#00ffd8'}}>
             <h1>Home</h1>
             <Search searchFilter = {this.searchFilter} />
-            {this.state.filteredDragons.map((dragon) => {
+            {filteredDragons.map((dragon) => {
                 return(
                     <div key={dragon.id} >
                         <h2 onClick={() => this.props.goToWar(dragon.id, dragon.atWar)} >{dragon.name}</h2>
--- src/components/War.js
+++ src/components/War.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
+import { Search } from './Search'
+
 
 class War extends Component{
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            query: ''
+        }
+    }
+
+    searchFilter = (query) => {
+        this.setState(state => {
+            state.query = query
+            return state
+        })
+    }
+
     render(){
+        let filteredDragons = this.props.dragons.filter(dragon => {
+            const result = dragon.name.toLowerCase().search(this.state.query.toLowerCase())
+            if (result === -1) {
+                return false
+            } else {
+                return true
+            }
+        })    
+
         // Note how style attributes are now passed as object instead of a string!!!
         return (
             <div style={{float:'left', width:'40%', padding:'5%', backgroundColor:'#f98181'}}>
                 <h1>War</h1>
-                {this.props.dragons.map((dragon) => {
+                <Search searchFilter={this.searchFilter} />
+                {filteredDragons.map((dragon) => {
                     return (
                         <div key={dragon.id} >
                             <h2 onClick={() => this.props.goToHome(dragon.id, dragon.atWar)} >{dragon.name}</h2>
